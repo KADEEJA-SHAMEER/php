053ebffdbc8d725eb48bb7c741621544e2317d4d
Kadeeja-kadu <kadeejashameer110@gmail.com>
2024-08-11 22:30:08 -0700

    updated

--- miniproject/home.php
+++ miniproject/home.php
@@ -1,16 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="homestyle.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    <div claas="div1">
-        <img src="./logo.svg" width=20%>
+    <div class="div1">
+        <img src="./logo.svg" width=20%></div>
+    <div >
         <form action="login.php" method="post">
-            <input type=submit name=submit value=LOGIN>
-</form>
+            <button type=submit name=login >LOGIN </button>
+       </form>
 </div> 
+<!--<div>
+    <h1> WHERE WORKS<BR>MEET FREEDOM</h1>
+</div>-->
 </body>
 </html>
